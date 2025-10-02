7e95de6e6dbb6928c228b14bc8723e12188e0b1b
Arkm20 <aarush.kkarnik@outlook.com>
2025-10-02 17:19:14 -0500

    Fixed public class name

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicTeleOP.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicTeleOP.java
@@ -14,7 +14,7 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 @TeleOp
-public class MecanumTeleOp extends LinearOpMode {
+public class BasicTeleOP extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
         // Motor Declaration
