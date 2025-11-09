4fb082c57f999b1789365e9b64550815421e2afb
Arkm <101435917+ogarkm@users.noreply.github.com>
2025-11-09 17:46:04 -0600

    Netzone MeepMeep

---
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepRedNetzone.java
@@ -0,0 +1,78 @@
+package com.example.meepmeeptesting;
+
+import com.acmerobotics.roadrunner.Pose2d;
+import com.noahbres.meepmeep.MeepMeep;
+import com.noahbres.meepmeep.core.colorscheme.scheme.ColorSchemeBlueDark;
+import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
+import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
+
+
+// #===================#
+//
+// Red Netzone MeepMeepVisualization
+//
+// #===================#
+
+public class MeepMeepRedNetzone {
+    public static void main(String[] args) {
+        MeepMeep meepMeep = new MeepMeep(800);
+
+        // === Blue Net Zone Bot (top-left of field) ===
+        RoadRunnerBotEntity blueNetBot = new DefaultBotBuilder(meepMeep)
+                .setConstraints(50, 50, Math.toRadians(180), Math.toRadians(180), 15)
+                .setDimensions(14, 16)
+                .setColorScheme(new ColorSchemeBlueDark())
+                .build();
+
+        blueNetBot.runAction(blueNetBot.getDrive().actionBuilder(
+                                // Start top-left (Blue Net side)
+                                new Pose2d(-56, 56, Math.toRadians(135))
+                        )
+
+                        .lineToY(12) // drive up into shooting position
+                        .waitSeconds(0.2)
+                        // shoot()
+                        .waitSeconds(3)
+
+
+
+                        // === PHASE 2: Collect from POS 2 (TOP row) ===
+                        .turnTo(Math.toRadians(90))
+                        .lineToY(35)
+                        .waitSeconds(0.5)
+                        // intake()
+                        .lineToY(45)
+
+                        .lineToY(13)
+                        .waitSeconds(0.3)
+                        // shoot()
+                        .waitSeconds(3)
+
+                        // === PHASE 3: Collect from POS 3 (Middle row) ===
+                        .turnTo(Math.toRadians(45))
+                        .lineToY(37)
+
+                        // intake() Start Intake
+                        .turnTo(Math.toRadians(90))
+                        .lineToY(45)
+                        .waitSeconds(0.3)
+
+                        .lineToY(35)
+                        .turnTo(Math.toRadians(225))
+
+                        .lineToY(13)
+                        // shoot()
+                        .waitSeconds(3)
+                        .lineToY(20)
+
+                        .build()
+        );
+
+        // Configure MeepMeep
+        meepMeep.setBackground(MeepMeep.Background.FIELD_DECODE_OFFICIAL)
+                .setDarkMode(true)
+                .setBackgroundAlpha(0.95f)
+                .addEntity(blueNetBot)
+                .start();
+    }
+}
